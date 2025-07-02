f07e75e2dd2a0ad49ffc4c018f6762a31d1ede28
Nguyen Luc <lucnguyen2407@gmail.com>
2025-07-02 14:05:04 +0700

    fix bug defined but never used

--- src/components/layout/left-side-bar-main.tsx
+++ src/components/layout/left-side-bar-main.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Globe, Plus } from "lucide-react";
 import { PromoCard } from "../common/promo-card";
--- src/components/layout/main-content.tsx
+++ src/components/layout/main-content.tsx
@@ -1,7 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardNoBorder } from "@/components/ui/card";
+import { CardContent, CardNoBorder } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { popularArtists, trendingSongs } from "@/constants/data";
 import Image from "next/image";
